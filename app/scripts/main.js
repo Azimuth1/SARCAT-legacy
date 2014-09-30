1cd10accb90435c8434aabea128e07af71bda1e7
Kyle <kkalwarski@gmail.com>
2014-09-30 15:37:25 -0400

    added login screen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,19 @@
 'use strict';
-//$('#myModal').modal();
 
 
-var generatePage = function(d) {
-    var tabs = d.config;
-    var appendInput = function(d, elem, horz) {
+
+
+var SARCAT = function() {
+    var sarcat = {};
+var appendInput = function(d, elem, horz) {
         var div = d3.select(elem);
         var type = d.type;
-        type = (type === 'select') ? 'radio' : type;
+        type = (type === 'select' && d.vals.length < 6) ? 'radio' : type;
         if (horz) {
-            div = div.append('div')
-                .attr('class', 'col-xs-6');
-            div.append('h4')
-                .attr('class', 'control-label')
-                .style('text-align', 'left')
-                .text(function() {
-                    return d.name;
-                });
+            div = div.append('div').attr('class', 'col-xs-6');
+            div.append('h4').attr('class', 'control-label').style('text-align', 'left').text(function() {
+                return d.name;
+            });
         }
         if (d.data) {
             d.data.forEach(function(e) {
@@ -25,143 +22,171 @@ var generatePage = function(d) {
             return;
         }
         if (type === 'select') {
-            var select = div.append('select')
-                .attr('class', 'form-control');
-            select.selectAll('option')
-                .data(d.vals)
-                .enter()
-                .append('option')
-                .attr('value', function(e) {
-                    return e;
-                })
-                .text(function(e) {
-                    return e;
-                });
+            var select = div.append('select').attr('class', 'form-control');
+            select.selectAll('option').data(d.vals).enter().append('option').attr('value', function(e) {
+                return e;
+            }).text(function(e) {
+                return e;
+            });
         }
         if (type === 'text') {
-            div.append('input')
-                .attr('type', 'text')
-                .attr('class', 'form-control')
-                .attr('placeholder', function() {
-                    return d.name;
-                });
+            div.append('input').attr('type', 'text').attr('class', 'form-control').attr('placeholder', function() {
+                return d.name;
+            });
         }
         if (type === 'number') {
-            div.append('input')
-                .attr('type', 'number')
-                .attr('class', 'form-control')
-                .attr('placeholder', function() {
-                    return d.name;
-                });
+            div.append('input').attr('type', 'number').attr('class', 'form-control').attr('placeholder', function() {
+                return d.name;
+            });
         }
         if (type === 'radio') {
-            div = div.append('div')
-                .classed('btn-group', true)
-                .attr('data-toggle', 'buttons');
-            var radios = div.selectAll('radio')
-                .data(d.vals);
-            var labels = radios.enter()
-                .append('label')
-                .attr('class', 'btn btn-primary');
+            div = div.append('div').classed('btn-group', true).attr('data-toggle', 'buttons');
+            var radios = div.selectAll('radio').data(d.vals);
+            var labels = radios.enter().append('label').attr('class', 'btn btn-primary');
             labels.text(function(d) {
                 return d;
             });
-            labels.append('input')
-                .attr('type', 'radio')
-                .attr('name', function() {
-                    return d.name;
-                });
-        }
-        if (type === 'date') {
-            div.append('input')
-                .attr('type', 'date')
-                .attr('class', 'form-control')
-                .attr('placeholder', function() {
-                    return d.name;
-                });
-        }
-        if (type === 'textarea') {
-            div.append('textarea')
-                .attr('class', 'form-control')
-                .attr('rows', '3')
-                .attr('placeholder', function() {
-                    return d.placeholder;
-                });
-        }
-    };
-    var formContainer = d3.select('.formContainer')
-        .append('div')
-        .attr('class', function() {
-            return 'col-md-12 formFill';
-        })
-        .selectAll('form')
-        .data(tabs);
-    var formData = formContainer.enter()
-        .append('form')
-        .attr('class', function(d, i) {
-            var vis = (i === 0) ? '' : '_hidden';
-            return 'form-horizontal ' + vis;
-        });
-    formData.append('legend')
-        .text(function(d) {
-            return d.name;
-        });
-    formData.select(function(d) {
-        var inputs = d.data || [];
-        var inputDiv = d3.select(this)
-            .selectAll('div')
-            .data(inputs)
-            .enter()
-            .append('div')
-            .attr('class', 'form-group pad5')
-            .append('div')
-            .attr('class', 'col-md-12 pad5 form-group-style');
-        inputDiv.append('label')
-            .attr('class', 'control-label col-xs-2')
-            .text(function(d) {
+            labels.append('input').attr('type', 'radio').attr('name', function() {
                 return d.name;
             });
-        inputDiv.append('div')
-            .attr('class', 'col-xs-10')
-            .select(function(d) {
+        }
+        if (type === 'date') {
+            div.append('input').attr('type', 'date').attr('class', 'form-control').attr('placeholder', function() {
+                return d.name;
+            });
+        }
+        if (type === 'textarea') {
+            div.append('textarea').attr('class', 'form-control').attr('rows', '3').attr('placeholder', function() {
+                return d.placeholder;
+            });
+        }
+    };
+var generatePage = function(d) {
+        var tabs = d.config;
+        var form = d3.select('body').append('div').attr('id', 'form-container').attr('class', 'container');
+        form.append('div').attr('class', 'page-header');
+        form.append('h3').html('SARCAT <small>Missing persons database entry.</small>');
+        var progress = form.append('div').attr('class', 'row pad0');
+        progress.append('div').attr('class', 'col-md-12 paxd5').append('div').attr('class', 'progress').append('div').attr('class', 'progress-bar progress-bar-info progress-bar-striped').text(
+            '0%');
+        progress.append('div').attr('class', 'col-md-12 pad5y').append('div').attr('class', 'step');
+        var formContainer = form.append('div').attr('class', 'row formContainer').append('div').attr('class', function() {
+            return 'col-md-12 formFill';
+        }).selectAll('form').data(tabs);
+        var formData = formContainer.enter().append('form').attr('class', function(d, i) {
+            var vis = (i === 0) ? 'step' + d.step : '_hidden step' + d.step;
+            return 'col-md-12 form-horizontal ' + vis;
+        });
+        formData.append('legend').text(function(d) {
+            return d.name;
+        });
+        formData.select(function(d) {
+            var inputs = d.data || [];
+            var inputDiv = d3.select(this).selectAll('div').data(inputs).enter().append('div').attr('class', 'form-group pad5').append('div').attr('class',
+                'col-md-12 pad5 form-group-style');
+            inputDiv.append('label').attr('class', 'control-label col-xs-2').text(function(d) {
+                return d.name;
+            });
+            inputDiv.append('div').attr('class', 'col-xs-10').select(function(d) {
                 appendInput(d, this);
             });
-    });
-    var steps = d3.select('.step')
-        .selectAll('p')
-        .data(tabs);
-    var step = steps.enter()
-        .append('div')
-        .attr('class', function(d, i) {
-            return i === 0 ? 'activestep steps' : 'steps';
-        })
-        .on('click', function(e, i) {
-            formContainer.classed('_hidden', true);
+        });
+        var steps = d3.select('.step').selectAll('p').data(tabs);
+        var step = steps.enter().append('div').attr('class', function(d, i) {
+            return i === 0 ? 'activestep steps btnStep' + i : 'steps btnStep' + i;
+        }).on('click', function(e, i) {
+            formContainer.classed('hidden', true);
             formContainer.each(function(d) {
                 if (d.step === e.step) {
-                    d3.select(this)
-                        .classed('_hidden', false);
+                    d3.select(this).classed('hidden', false);
                 }
             });
             var elem = d3.select(this);
             var percent = i * 10;
-            d3.select('.progress-bar')
-                .style('width', percent + '%')
-                .attr('aria-valuenow', percent)
-                .text(percent + '%');
+            d3.select('.progress-bar').style('width', percent + '%').attr('aria-valuenow', percent).text(percent + '%');
             step.classed('activestep', false);
             elem.classed('activestep', true);
         });
-    step.append('span')
-        .attr('class', 'glyphicon glyphicon-ok btn-md');
-    step.append('h6')
+        step.append('span').attr('class', 'glyphicon glyphicon-ok btn-md');
+        step.append('h6').text(function(d) {
+            return d.name;
+        });
+    };
+var addMap = function(elem, callback) {
+        var map = elem.insert('div', ':first-child').attr('id', 'map').style({
+            height: '500px',
+            width: '100%'
+        });
+        L.mapbox.accessToken = 'pk.eyJ1IjoibWFwcGlza3lsZSIsImEiOiJ5Zmp5SnV3In0.mTZSyXFbiPBbAsJCFW8kfg';
+        L.mapbox.map('map', 'examples.map-i86nkdio').setView([40, -74.50], 9);
+        if (callback) {
+            callback();
+        }
+    };
+sarcat.dashboard = function() {
+        d3.json('data/login.json', function(d) {
+            d3.select('.wrapper').style({
+                display: 'none'
+            });
+            var data = d.login;
+            var elem = d3.select('.login').append('div').attr('class', 'form-welcome');
+            elem.append('h1').text('Welcome User!');
+            elem.append('hr');
+            /*
+elem.append('h3').text('Recent Records');
+    var list = elem.append('div').attr('class', 'col-md-12').append('div')
+        .attr('class', 'list-group')
+        .selectAll('a')
+        .data(data);
+    var a = list.enter()
+        .append('a')
+        .attr('href', '#')
+        .attr('class', function(d) {
+            var status = d.status == 'incomplete' ? 'list-group-item-danger' : 'list-group-item-success';
+            return 'list-group-item ' + status;
+        });
+    a.append('h4')
+        .attr('class', 'list-group-item-heading')
         .text(function(d) {
             return d.name;
         });
+    a.append('div')
+        .attr('class', 'list-group-item-text')
+        .text(function(d) {
+            return d.date;
+        });*/
+            elem.append('button').attr('type', 'button').attr('class', 'btn btn-primary btn-lg btn-block').text('Create New Record').on('click', function() {
+                sarcat.generateForm();
+            });
+            elem.append('button').attr('type', 'button').attr('class', 'btn btn-default btn-lg btn-block').text('Edit Existing Record');
+        });
+    };
+    sarcat.generateForm = function() {
+        d3.json('data/config.json', function(d) {
+            d3.select('.login').style({
+                display: 'none'
+            });
+            generatePage(d);
+            addMap(d3.select('.step2'), function() {
+                $('.btnStep0').click();
+            });
+        });
+    };
+    return sarcat;
 };
 
 
 
-d3.json('data/config.json',function(d){generatePage(d);});
 
-
+(function() {
+    var sarcat = SARCAT();
+    d3.select('.demoLogin').on('click', function() {
+        sarcat.dashboard();
+        /*sarcat.generateForm();
+        window.location.hash = '#sarcat';
+        location.reload();*/
+    });
+    if (window.location.hash == '#sarcat') {
+        sarcat.generateForm();
+    }
+})();
