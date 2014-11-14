3cda2823b00b9e89d09e351a9407fb7cec85a677
KyleK <kkalwarski@gmail.com>
2014-11-14 13:42:42 -0500

    database tests

--- config/sql.js
+++ config/sql.js
@@ -1,7 +1,7 @@
 module.exports = function(context) {
   var sqlite3 = require('sqlite3').verbose();
   var fs = require("fs");
-
+var db;
   var filename = context + ".db";
 
   var db = new sqlite3.Database(filename);
@@ -10,12 +10,14 @@ module.exports = function(context) {
   var exists = fs.existsSync(filename);;
 
   login.newTable = function() {
-    if(exists){return;}
+    if(exists){console.log('table already exists!');return;}
+    
     db.run("CREATE TABLE login (username test, password text)");
     db.close();
   };
   login.insert = function(a, b) {
     db.run("INSERT INTO login VALUES ('"+a+"', '"+b+"')");
+    console.log('items inserted!')
     db.close();
   };
   login.query = function() {
