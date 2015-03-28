b4a5099b21560d32a1f493312cfe4b1803610cd1
Kyle Kalwarski <kkalwarski@gmail.com>
2015-03-28 02:49:38 -0400

    layout

--- meteor_SARCAT/client/main.js
+++ meteor_SARCAT/client/main.js
@@ -1,4 +1,4 @@
-getLocation = function(cb) {
+getLocation = function (cb) {
     var result;
 
     function requestLocation() {
@@ -38,15 +38,54 @@ getLocation = function(cb) {
 
 }
 
-setMap = function(context, coords, popup) {
-    console.log(coords)
-    coords = [coords.lat, coords.lng];
-    console.log(coords)
-
+setMap = function (context, coords, popup) {
     var map = L.map(context)
-        .setView(coords, 13);
+        .setView([coords.lat, coords.lng], coords.zoom);
     m = map;
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            id: 'examples.map-i875mjb7'
+        })
+        .addTo(map);
 
+    map.on('move', function () {
+        var position = map.getCenter();
+        var zoom = map.getZoom()
+        $('[name="agencyProfile.coordinates.lng"]')
+            .val(position.lng);
+        $('[name="agencyProfile.coordinates.lat"]')
+            .val(position.lat);
+        $('[name="agencyProfile.coordinates.zoom"]')
+            .val(zoom);
+    });
+    /*
+        var ippMarker = L.marker([m.getCenter()
+            .lat, m.getCenter()
+            .lng
+        ], {
+            draggable: true
+        });
+        ippMarker.addTo(map);
+        ippMarker.on('drag', function(event) {
+            var marker = event.target;
+            var position = marker.getLatLng();
+            $('[name="agencyProfile.coordinates.lng"]')
+                .val(position.lng);
+            $('[name="agencyProfile.coordinates.lat"]')
+                .val(position.lat);
+
+        });
+        ippMarker.bindPopup('<b>' + popup + '</b>', {
+                noHide: true
+            })
+            .openPopup();*/
+    return map;
+}
+
+newProjectSetMap = function (context, coords, popup) {
+    var map = L.map(context)
+        .setView([coords.lat, coords.lng], coords.zoom);
+    m = map;
     L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
             maxZoom: 18,
             id: 'examples.map-i875mjb7'
@@ -60,7 +99,7 @@ setMap = function(context, coords, popup) {
         draggable: true
     });
     ippMarker.addTo(map);
-    ippMarker.on('drag', function(event) {
+    ippMarker.on('drag', function (event) {
         var marker = event.target;
         var position = marker.getLatLng();
         $('[name="agencyProfile.coordinates.lng"]')
@@ -73,5 +112,76 @@ setMap = function(context, coords, popup) {
             noHide: true
         })
         .openPopup();
+    return map;
+}
+
+formSetMap = function (context, coords, points) {
+    var center = [coords.lat, coords.lng];
+    var map = L.map(context)
+        .setView(center, coords.zoom);
+    m = map;
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            id: 'examples.map-i875mjb7'
+        })
+        .addTo(map);
+
+    var ne = m.getBounds()._northEast;
+    var sw = m.getBounds()._southWest;
+    var n = ne.lat;
+    var e = ne.lng;
+    var w = sw.lng;
+    var diff = ne.lng - sw.lng;
+    var factor = diff / (points.length);
+    console.log(factor)
+    var newLat = n - ((n - coords.lat) * .2);
+    points.forEach(function (d, i) {
+        var newLng = w + (factor * i) + (factor / 3);
+        var marker = L.marker([newLat, newLng], {
+                draggable: true,
+                className:'z2'
+            });
+
+            marker.bindLabel(d.text, {
+                noHide: true
+            })
+            .addTo(map);
+
+
+            marker.on('drag', function (event) {
+                var marker = event.target;
+                var position = marker.getLatLng();
+                $('[name="'+d.name+'.lng"]')
+                    .val(position.lng);
+                $('[name="'+d.name+'.lat"]')
+                    .val(position.lat);
+
+            });
+        /*
+
+            var marker = L.marker([coords.lat, coords.lng], {
+                draggable: true
+            });
+
+            marker.bindLabel("My Label", {noHide: true, className: "my-label", offset: [0, 0] });
+            marker.addTo(map);
+            marker.on('drag', function (event) {
+                var marker = event.target;
+                var position = marker.getLatLng();
+                $('[name="'+d.name+'.lng"]')
+                    .val(position.lng);
+                $('[name="'+d.name+'.lat"]')
+                    .val(position.lat);
+
+            });
+            marker.bindPopup('<b>' + d.text + '</b>', {
+                    noHide: true
+                })
+                .openPopup();
+
+        */
+
+    });
+
     return map;
 }
--- meteor_SARCAT/client/templates/admin.js
+++ meteor_SARCAT/client/templates/admin.js
@@ -1,13 +1,12 @@
-var agencyProfile;
-var agencyCoordinates;
-var map;
+//var agencyProfile;
+//var agencyCoordinates;
+//var map;
 var mapDrawn;
-var config = Session.get('config');
-Template.admin.created = function() {
-    agencyProfile = Session.get('config').agencyProfile;
+var config;
+Template.admin.created = function () {
+    config = Session.get('config');
 
-
-    agencyCoordinates = agencyProfile.coordinates;
+    /*agencyCoordinates = agencyProfile.coordinates;
 
     if (!agencyCoordinates) {
         getLocation(function(coords, err) {
@@ -27,17 +26,13 @@ Template.admin.created = function() {
             }
 
         })
-    }
+    }*/
 
 };
 
-Template.admin.rendered = function() {
-    if (agencyCoordinates && !mapDrawn) {
-        setMap('adminMap', agencyProfile.coordinates,'Drag me to set your default Home Base');
-        mapDrawn = true;
-    }
+Template.admin.rendered = function () {
 
-    this.data.users.forEach(function(d) {
+    this.data.users.forEach(function (d) {
         var role = d.roles[0];
         var id = d._id;
         $('input[name="role_' + id + '"][value="' + role + '"]')
@@ -45,50 +40,50 @@ Template.admin.rendered = function() {
     });
 };
 Template.admin.helpers({
-    agencyCoordinates: function() {
+    agencyCoordinates: function () {
         return agencyCoordinates;
     },
-    profileIncomplete: function() {
+    profileIncomplete: function () {
         var complete = completeProfile();
         Session.set('profileComplete', complete);
         return !complete;
     },
-    configs: function() {
+    configs: function () {
         return Session.get('config');
     },
-    userEmail: function() {
+    userEmail: function () {
         return this.emails[0].address;
     },
-    removeUser: function(a) {
+    removeUser: function (a) {
         console.log(this, a)
-        Meteor.call('removeUser', this._id, function(err) {
+        Meteor.call('removeUser', this._id, function (err) {
             console.log(err);
         });
     },
-    test1: function() {
+    test1: function () {
         return Schemas.SARCAT._schemaKeys;
     },
-    test2: function() {
+    test2: function () {
         return Schemas.SARCAT._firstLevelSchemaKeys;
     },
-    test3: function(name) {
+    test3: function (name) {
         var schema = Schemas[name];
         if (!schema) {
             return [];
         }
-        return Schemas[name]._firstLevelSchemaKeys.map(function(d) {
+        return Schemas[name]._firstLevelSchemaKeys.map(function (d) {
             return name + '.' + d;
         });
     },
-    arrRecords: function() {
+    arrRecords: function () {
         if (!this.record) {
             return;
         }
         var record = this.record;
         return _.chain(record)
-            .map(function(d, key) {
+            .map(function (d, key) {
                 if (_.isObject(d)) {
-                    return _.map(d, function(d2, key2) {
+                    return _.map(d, function (d2, key2) {
                         return {
                             name: key + '.' + key2,
                             //name: 'Schemas.'+key2,
@@ -101,7 +96,7 @@ Template.admin.helpers({
             .compact()
             .value();
     },
-    roleIsChecked: function(e, f) {
+    roleIsChecked: function (e, f) {
         return true;
         return this.roles[0] === $(e.target)
             .val();
@@ -112,26 +107,42 @@ Template.admin.helpers({
     },
 });
 Template.admin.events({
-    'click .removeUser': function(event, template) {
+    'click .removeUser': function (event, template) {
 
-        if(Meteor.userId()===this._id){alert('You cannot remove your own account!');return;}
-        Meteor.call('removeUser', this._id, function(err) {
+        if (Meteor.userId() === this._id) {
+            alert('You cannot remove your own account!');
+            return;
+        }
+        Meteor.call('removeUser', this._id, function (err) {
             console.log(err);
         });
     },
-    'change .adminUserRoles': function(event) {
+    'click .adminMap': function (event, template) {
+        if (mapDrawn) {
+            return;
+        }
+
+        var agencyProfile = config.agencyProfile;
+        setTimeout(function (d) {
+            setMap('adminMap', agencyProfile.coordinates);
+        }, 500);
+        mapDrawn = true;
+        return;
+
+    },
+    'change .adminUserRoles': function (event) {
         var user = this._id;
-        
-                if (Meteor.userId() === this._id) {
-                    alert('You cannot remove your own account!');
-                    return;
-                }
+
+        if (Meteor.userId() === this._id) {
+            alert('You cannot remove your own account!');
+            return;
+        }
         var val = $('input[name="role_' + user + '"]:checked')
             .val();
         console.log(user, val)
             //var checked = event.target.checked;
             //var val = $(e.target).val();
-        Meteor.call('changeRole', user, val, function(err) {
+        Meteor.call('changeRole', user, val, function (err) {
             console.log(err);
         });
         //$('input[name="role_oS8Y6oZC5WraaCnPW"]:checked').val();
@@ -139,7 +150,7 @@ Template.admin.events({
     }
 });
 hooks2 = {
-    onSubmit: function(doc) {
+    onSubmit: function (doc) {
         console.log(doc);
         Schemas.SARCAT.clean(doc);
         console.log(doc);
@@ -147,17 +158,17 @@ hooks2 = {
         return false;
     },
     // Schemas.SARCAT.clean(doc);
-    onSuccess: function(formType, result) {
+    onSuccess: function (formType, result) {
         console.log(formType, result);
     },
-    onError: function(formType, error) {
+    onError: function (formType, error) {
         console.log(formType, error);
     },
-    beginSubmit: function(a) {
+    beginSubmit: function (a) {
         // console.log()
         console.log('beginSubmit');
     },
-    endSubmit: function() {
+    endSubmit: function () {
         console.log('endSubmit');
     }
 };
