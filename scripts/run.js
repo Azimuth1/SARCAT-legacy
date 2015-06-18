202fd4778ec4aeb774525323c62a25b76944fc89
Kyle Kalwarski <kkalwarski@gmail.com>
2015-06-18 09:27:21 -0400

    build scripts
    Former-commit-id: de9bc4dd0ae66c6b76b21d590f5c710d662d4bf2

---
+++ scripts/run.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+// Generic node.js execution wrapper to exclude win32 from runs
+// without failing on windows.
+
+if (process.platform !== 'win32') runscript();
+
+function runscript() {
+    var execFile = require('child_process').execFile;
+    var path = require('path');
+
+    var file = path.resolve(process.argv[2]);
+
+    execFile(file, [], {}, function(err, stdout, stderr) {
+        console.log(stdout);
+        console.warn(stderr);
+        if (err && err.code) {
+            process.exit(err.code);
+        }
+        process.exit(0);
+    });
+}
